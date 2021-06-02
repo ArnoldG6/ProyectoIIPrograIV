a0eccab5e1099d48f8bf97ffcfc3a8cd728cebf4
Miguelgonz98 <mgonzalex236@gmail.com>
2021-06-02 15:53:40 -0600

    RoomCRUD (problems to recover int hashmap) 15:53

--- ProyectoIIPrograIV/src/java/model/entities/RoomDAO.java
+++ ProyectoIIPrograIV/src/java/model/entities/RoomDAO.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.entities;
+import java.util.HashMap;
+import model.Room;
 
-/**
- *
- * @author GONCAR4
- */
-public class RoomDAO {
+public class RoomDAO implements DAO<String, Room> {
+
+    @Override
+    public HashMap<String, Room> listAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void add(String id, Room value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Room recover(String id, String pass) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(String id, Room value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
