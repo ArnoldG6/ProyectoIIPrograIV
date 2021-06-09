fae0b8fd58dd8a80a9b7ba7bed9e0dc56f6e93c6
Miguelgonz98 <mgonzalex236@gmail.com>
2021-06-09 15:45:32 -0600

    Creation of login class 15:53

---
+++ ProyectoIIPrograIV/src/java/model/presentation/Login.java
@@ -0,0 +1,18 @@
+package model.presentation;
+
+
+import javax.annotation.security.PermitAll;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+public class Login {
+    
+}
