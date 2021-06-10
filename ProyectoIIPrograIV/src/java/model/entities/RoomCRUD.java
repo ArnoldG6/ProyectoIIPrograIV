176470032af6822884db0be25a87366b771b4968
arnoldgq612 <arnoldgq612@gmail.com>
2021-06-10 11:06:49 -0600

    11:06am

--- ProyectoIIPrograIV/src/java/model/entities/RoomCRUD.java
+++ ProyectoIIPrograIV/src/java/model/entities/RoomCRUD.java
@@ -2,22 +2,15 @@ package model.entities;
 
 public class RoomCRUD {
     protected static final String CMD_LIST
-            = "SELECT  FROM room;";
+            = "SELECT * FROM room;";
     protected static final String CMD_ADD
             = "INSERT INTO room ( ) "
             + "VALUES (?, ?); ";
     protected static final String CMD_RECOVER
-            = "SELECT  FROM room"
-            + "WHERE  = ?; ";
-    protected static final String CMD_UPDATE_USERNAME
-            = "UPDATE room SET  = ?"
-            + "WHERE  = ?; ";
-    protected static final String CMD_UPDATE_ADMIN_ID
-            = "UPDATE room SET  = ?"
+            = "SELECT * FROM room"
             + "WHERE  = ?; ";
     protected static final String CMD_DELETE
             = "DELETE FROM room "
             + "WHERE  = ?; ";
-    protected static final String CMD_COUNT
-            = "SELECT COUNT(*) AS total_room FROM room;";
+
 }
