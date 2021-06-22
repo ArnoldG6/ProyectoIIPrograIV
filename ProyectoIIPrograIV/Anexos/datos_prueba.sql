cec5d289f7ab06cdf12ca526d59c05f97e015a09
arnoldgq612 <arnoldgq612@gmail.com>
2021-06-22 17:58:23 -0600

    5.58pm

---
+++ ProyectoIIPrograIV/Anexos/datos_prueba.sql
@@ -0,0 +1,4 @@
+insert into cinema.movies(id,description,publicationYear,director,
+inBillboard,name,imgLink) values(
+4,'Shrek viene en busca de aventuras a Costa Rica', '2021', 'Antonio Guzmán',
+'SI','Shrek Tico', 'XD');
